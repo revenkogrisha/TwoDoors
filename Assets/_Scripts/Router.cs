73b7ea60dbb1d1037e5a8065208707c4c296df2f
Grisha Revenko <grisha@revenko.org>
2022-08-12 13:03:00 +0300

    Added Route() overload

--- Assets/_Scripts/Router.cs
+++ Assets/_Scripts/Router.cs
@@ -5,12 +5,31 @@ namespace TwoDoors
 {
     public static class Router
     {
-        public static void Route<T>(Action<T> handler, Collider2D contaiter)
+        public static bool Route<T>(Collider2D contaiter, Action<T> handler)
         {
             if (contaiter.GetComponent<T>() != null)
+            {
                 handler?.Invoke(
                     contaiter.GetComponent<T>()
                     );
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool Route<T>(Collider2D contaiter, params Action[] handlers)
+        {
+            if (contaiter.GetComponent<T>() != null)
+            {
+                foreach (var handler in handlers)
+                    handler?.Invoke();
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
