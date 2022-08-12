779cc8ab4f6ebacbfff9efa2a7dbd83683ce2757
Grisha Revenko <grisha@revenko.org>
2022-08-12 13:57:07 +0300

    Added BGMusic & other sounds

---
+++ Assets/_Scripts/Scene/BackgroundMusic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace TwoDoors.Scene
+{
+    public class BackgroundMusic : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _music;
+
+        private void Awake()
+        {
+            _music.Play();
+            DontDestroyOnLoad(this);
+        }
+    }
+}
