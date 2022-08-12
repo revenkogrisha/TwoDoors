b12e40dc3224944d29de4c85301ac4105ea38815
Grisha Revenko <grisha@revenko.org>
2022-08-12 14:35:24 +0300

    Bugfix: more than one music object on scene

--- Assets/_Scripts/Scene/BackgroundMusic.cs
+++ Assets/_Scripts/Scene/BackgroundMusic.cs
@@ -8,8 +8,21 @@ namespace TwoDoors.Scene
 
         private void Awake()
         {
+            CheckMusicPresence();
             _music.Play();
             DontDestroyOnLoad(this);
+        }
+
+        private void CheckMusicPresence()
+        {
+            var musicObjects = FindObjectsOfType<BackgroundMusic>();
+            foreach (var item in musicObjects)
+            {
+                if (item.Equals(this))
+                    continue;
+
+                Destroy(item.gameObject);
+            }
         }
     }
 }
