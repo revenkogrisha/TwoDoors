b72dde781b0b9382f0a40da2f5b0918f4e69e067
Grisha Revenko <grisha@revenko.org>
2022-08-15 12:17:30 +0300

    Added Boss animation

---
+++ Assets/_Scripts/Scene/BossAnimator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace TwoDoors.Scene
+{
+    [RequireComponent(typeof(Animator))]
+    public class BossAnimator : MonoBehaviour
+    {
+        private const string IsTriggered = nameof(IsTriggered);
+
+        [Inject] private Score _score;
+
+        private Animator _animator;
+        private int _isTriggeredId;
+
+        #region MonoBehaviour
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _isTriggeredId = Animator.StringToHash(IsTriggered);
+        }
+
+        private void OnEnable()
+        {
+            if (_score is LevelScore levelScore)
+                levelScore.OnPlayerFailed += StartPunishment;
+        }
+
+        private void OnDisable()
+        {
+            if (_score is LevelScore levelScore)
+                levelScore.OnPlayerFailed -= StartPunishment;
+        }
+
+        #endregion
+
+        private void StartPunishment() => _animator.SetBool(_isTriggeredId, true);
+
+        private void EndPunishment() => _animator.SetBool(_isTriggeredId, false);
+    }
+}
