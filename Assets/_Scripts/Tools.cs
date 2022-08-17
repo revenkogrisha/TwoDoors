d56b640fb8cc98bc3276d95ddf51dc77e9fc9f82
Grisha Revenko <grisha@revenko.org>
2022-08-17 18:58:47 +0300

    Updated Tools

--- Assets/_Scripts/Tools.cs
+++ Assets/_Scripts/Tools.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace TwoDoors
 {
     public static class Tools
     {
-        public static bool DoIfIsNotNull<T>(Collider2D contaiter, Action<T> handler)
+        public static bool InvokeIfNotNull<T>(Collider2D contaiter, Action<T> handler)
         {
             if (contaiter.GetComponent<T>() != null)
             {
@@ -19,7 +20,7 @@ namespace TwoDoors
             return false;
         }
 
-        public static bool DoIfIsNotNull<T>(Collider2D contaiter, params Action[] handlers)
+        public static bool InvokeIfNotNull<T>(Collider2D contaiter, params Action[] handlers)
         {
             if (contaiter.GetComponent<T>() != null)
             {
@@ -30,6 +31,20 @@ namespace TwoDoors
             }
 
             return false;
+        }
+
+        public static void InvokeWithSameArgs<T>(T arg, params Action<T>[] actions)
+        {
+            foreach (var action in actions)
+                action?.Invoke(arg);
+        }
+
+        public static void InvokeWithChance(int percentChance, params Action[] actions)
+        {
+            var random = Random.Range(0, 101);
+            if (random <= percentChance)
+                foreach (var action in actions)
+                    action?.Invoke();
         }
     }
 }
